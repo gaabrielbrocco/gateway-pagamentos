c76f55c5bf313a069b7312291b0f2351bef9e40f
Gabriel <gabriel@webcgi.com.br>
2025-04-14 13:22:12 -0300

    ✨ feat(server): configura rotas e integra serviço de invoice

--- go-gateway-api/cmd/app/main.go
+++ go-gateway-api/cmd/app/main.go
@@ -48,9 +48,12 @@ func main() {
 	accountRepository := repository.NewAccountRepository(db)
 	accountService := service.NewAccountService(accountRepository)
 
+	invoiceRepository := repository.NewInvoiceRepository(db)
+	invoiceService := service.NewInvoiceService(invoiceRepository, *accountService)
+
 	// configura e inicia o servidor HTTP
 	port := getEnv("HTTP_PORT", "8080")
-	srv := server.NewServer(accountService, port)
+	srv := server.NewServer(accountService, invoiceService, port)
 	srv.ConfigureRoutes()
 
 	if err := srv.Start(); err != nil {
